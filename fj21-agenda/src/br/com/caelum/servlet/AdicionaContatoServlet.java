bb52edb853ba9189def1c1c096df19b7f778192e
Johnatta <johnatta.costa@outlook.com>
2016-06-24 21:04:48 -0300

    incluindo MVC NO PROJETO

--- fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
+++ fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -54,10 +55,7 @@ public class AdicionaContatoServlet extends HttpServlet {
 		
 		//imprime o nome do contato que foi adicionado
 		
-		out.println("<html>");
-		out.println("<body>");
-		out.println("Contato " + contato.getNome() + " Adicionado com sucesso");
-		out.println("</body>");
-		out.println("</html>");
+		RequestDispatcher rd = request.getRequestDispatcher("/contato-adiconado.jsp");
+		rd.forward(request, response);
 	}
 }
